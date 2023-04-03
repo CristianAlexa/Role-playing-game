d99ad178fc659742b9610ce350c5425ec48d561f
CristianAlexa <cristian.alexa@gmail.com>
2023-04-03 22:19:45 +0100

    lesson 9
    lesson 9 completed

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-/*
-Challenge: 
-1. Take the hard-coded HTML for the Wizard card, bring it 
-   into index.js and then inject it back into its div with 
-   JavaScript.
-2. Do the same for Orc card. 
-*/
-
 const HERO = {
    elementId: "hero",
    name: "Wizard",
    avatar: "images/wizard.png",
    health: 60,
-   diceRoll: 6
+   diceRoll: 6, 
+   diceCount: 3
 }
 
 const MONSTER = {
@@ -19,16 +12,25 @@ const MONSTER = {
    name: "Orc",
    avatar: "images/orc.png",
    health: 10,
-   diceRoll: 4
+   diceRoll: 4, 
+   diceCount: 1
 }
 
 function renderCharacter(data) {
-   document.getElementById(data.elementId).innerHTML= `
+   let diceHtml = ''
+   const {elementId, name, avatar, health, diceRoll, diceCount} = data
+   for (let i = 0; i<diceCount; i++) {
+      diceHtml +=`<div class="dice">6</div>`
+   }
+
+   document.getElementById(elementId).innerHTML= `
                <div class="character-card">
-                    <h4 class="name"> ${data.name} </h4>
-                    <img class="avatar" src="${data.avatar}"/>
-                    <p class="health">health: <b> ${data.health} </b></p>
-                    <div class="dice-container"><div class="dice"> ${data.diceRoll} </div></div>
+                    <h4 class="name"> ${name} </h4>
+                    <img class="avatar" src="${avatar}"/>
+                    <p class="health">health: <b> ${health} </b></p>
+                    <div class="dice-container">
+                    ${diceHtml} 
+                    </div>
                 </div> 
                `
 }
